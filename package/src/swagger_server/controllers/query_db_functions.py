dc2467314a0d658189a5ae325bdcfd5b52c25d22
Sabine <sabine.bertram@mailbox.org>
2019-02-18 19:57:20 -0800

    feat: additional event parameters, update notification status

--- package/src/swagger_server/controllers/query_db_functions.py
+++ package/src/swagger_server/controllers/query_db_functions.py
@@ -10,7 +10,7 @@ from .authentication import (get_token_auth_header, requires_auth,
                              requires_scope)
 from .helpers import check_id
 
-BIGCHAINDB_URL = env.get('BIGCHAINDB_URL')
+BIGCHAINDB_URL = env.get('BIGCHAINDB_URL', 'http://example.com')
 
 client = MongoClient('mongodb://mongodb:27017/')
 db = client.query_database
@@ -21,26 +21,8 @@ query_details = db.query_details
 def post_query(body):
     token = get_token_auth_header()
     query = body.get('query')
-    details = query.get('details')
-    query_results = []
-    for item in details:
-        query_result = {
-            'university_id': item.get('university_id'),
-            'degree_id': item.get('degree_id'),
-            'course_id': item.get('course_id')
-        }
-        try:
-            if item.get('course_id'):
-                query_result['result'] = _query('course', item.get('course_id'), item, token)
-            elif item.get('degree_id'):
-                query_result['result'] = _query('degree', item.get('degree_id'), item, token)
-            else:
-                query_result['result'] = {}
-        except ValueError as exp:
-            return {"ERROR": exp}
-        query_results.append(query_result)
-    query['results'] = query_results
-    query['responses'] = _notify_students(query_results)
+    query['results'] = _query(query.get('details'), token)
+    query['responses'] = _notify_students(query['results'])
     query['timestamp'] = datetime.datetime.utcnow().strftime('%Y-%m-%d %H:%M')
     body['query'] = query
     return str(query_details.insert_one(body).inserted_id)
@@ -52,7 +34,8 @@ def get_query(id):
     result = query_details.find_one({'_id': ObjectId(id)})
     if result:
         result['_id'] = str(result['_id'])
-        return result
+        metrics_result = _compute_ratios([result])[0]
+        return metrics_result
     else:
         return {'ERROR': 'No matching data found.'}, 409
 
@@ -61,11 +44,40 @@ def get_query(id):
 def get_queries_by_customer(customer_id):
     result = query_details.find({'customer_id': customer_id})
     if result:
-        return _stringify_object_id(result)
+        metrics_result = _compute_ratios(result)
+        return _stringify_object_id(metrics_result)
     else:
         return {'ERROR': 'No matching data found.'}, 409
 
-def _query(_type, _id, item, token):
+def update_status(body):
+    result = query_details.find_one({'_id': ObjectId(body.get('id'))})
+    if not result:
+        return {'ERROR': 'No matching data found.'}, 409
+    else:
+        student_record = _set_status(body, result)
+        return query_details.update_one({'_id': ObjectId(body.get('id'))}, {'$set': {'query.responses.' + body.get('student_id'): student_record}}, upsert=False)
+
+def _query(details, token):
+    query_results = []
+    for item in details:
+        query_result = {
+            'university_id': item.get('university_id'),
+            'degree_id': item.get('degree_id'),
+            'course_id': item.get('course_id')
+        }
+        try:
+            if item.get('course_id'):
+                query_result['result'] = _query_bigchaindb('course', item.get('course_id'), item, token)
+            elif item.get('degree_id'):
+                query_result['result'] = _query_bigchaindb('degree', item.get('degree_id'), item, token)
+            else:
+                query_result['result'] = {}
+        except ValueError as exp:
+            return {"ERROR": exp}
+        query_results.append(query_result)
+    return query_results
+
+def _query_bigchaindb(_type, _id, item, token):
     headers = {'Authorization': 'Bearer ' + token}
     if item.get('absolute'):
         payload = {_type + '_id': _id, 'x': item.get('absolute')}
@@ -88,8 +100,36 @@ def _notify_students(query_results):
     notifications = {}
     for result in query_results:
         for student in result['result']:
-            notifications[student['student_address']] = 'sent'
+            notifications[student['student_address']] = {
+                'sent': datetime.datetime.utcnow().strftime('%Y-%m-%d %H:%M'),
+                'viewed': '',
+                'responded': '',
+                'accepted': False
+            }
     return notifications
+
+def _compute_ratios(results):
+    for result in results:
+        responses = result.get('query').get('responses')
+        viewed = responded = accepted = 0
+        for _, value in responses.items():
+            if value['viewed']:
+                viewed += 1
+            if value['responded']:
+                responded += 1
+            if value['accepted']:
+                accepted += 1
+        result['query']['metrics'] = {'viewed': viewed / len(responses), 'responded': responded / len(responses), 'accepted': accepted / len(responses)}
+    return results
+
+def _set_status(body, result):
+    student_record = result.get('query').get('responses').get(body.get('student_address'))
+    if 'viewed' in body:
+        student_record['viewed'] = datetime.datetime.utcnow().strftime('%Y-%m-%d %H:%M')
+    elif 'accepted' in body:
+        student_record['responded'] = datetime.datetime.utcnow().strftime('%Y-%m-%d %H:%M')
+        student_record['accepted'] = body.get('accepted')
+    return student_record
 
 def _stringify_object_id(result):
     stringified_result = []
