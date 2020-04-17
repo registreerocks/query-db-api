7db295ed72816c7ea50bbeba0ab213231cc655ec
Sabine <sabine.bertram@mailbox.org>
2020-04-17 17:15:35 +0200

    feat: password protect mongo db

--- package/src/swagger_server/controllers/db.py
+++ package/src/swagger_server/controllers/db.py
@@ -1,5 +1,11 @@
+from os import environ as env
+
 from pymongo import MongoClient
 
-CLIENT = MongoClient('mongodb://mongodb:27017/')
+CLIENT = MongoClient(
+  'mongodb://mongodb:27017/', 
+  username=env.get('MONGO_USERNAME'), 
+  password=env.get('MONGO_PASSWORD')
+  )
 DB = CLIENT.database
 query_details = DB.query_db
