c2afb28fcc08910202856031f36218c1b915ed02
Sabine <sabine.bertram@mailbox.org>
2020-05-26 10:34:45 +0200

    1.4.0

--- package/setup.py
+++ package/setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
    name='query_db',
-   version='1.3.1',
+   version='1.4.0',
    maintainer='Sabine Bertram',
    maintainer_email='sabine.bertram@mailbox.org',
    package_dir={'': 'src'},
