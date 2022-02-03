9559f725533a76d899b26d956d70682eb3c0f6e0
Herman <herman@registree.io>
2022-02-03 12:57:19 +0200

    feat: add function to get all records above an average (#42) (#43)
    * feat: add function to get all records above an average
    
    * feat: add check for average queries and update schema
    
    Co-authored-by: Sabine Bertram <sabine@registree.io>

--- package/src/swagger_server/controllers/query.py
+++ package/src/swagger_server/controllers/query.py
@@ -7,6 +7,8 @@ def _query_bulk(query_list):
     for item in query_list:
         if item.get('absolute'):
             responses[item.get('type_id')] = _get_top_x(item.get('x'), item.get('type'), item.get('type_id'))
+        elif item.get('average'):
+            responses[item.get('type_id')] = _get_all_above_average(item.get('x'), _item.get('type'), item.get('type_id'))
         else:
             responses[item.get('type_id')] = _get_top_x_percent(item.get('x'), item.get('type'), item.get('type_id'))
     return responses
@@ -21,6 +23,11 @@ def _get_top_x(x, _type, _id):
 def _get_top_x_percent(x, _type, _id):
     averages = _retrieve_averages(_type, _id)
     reduced_list = _x_percent(x, averages)
+    return dict((item.pop('student_address'), item.copy()) for item in reduced_list)
+
+def _get_all_above_average(average, _type, _id):
+    averages = _retrieve_averages(_type, _id)
+    reduced_list = [a for a in averages if a['avg'] >= average]
     return dict((item.pop('student_address'), item.copy()) for item in reduced_list)
 
 def _retrieve_averages(_type, _id):
