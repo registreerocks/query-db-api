b7d86fd8be3bc6d025d5fc5d900d6f24de1e99bc
Herman <herman@registree.io>
2022-02-08 10:11:27 +0200

    feat: add demographic filters to queries (#44)
    * feat: add demographic filters to queries
    
    * feat: update swagger.yaml to include demographic fields
    
    * feat: modify demographic queries to default to "Other"

--- package/test/test_create.py
+++ package/test/test_create.py
@@ -5,7 +5,7 @@ import mock
 import pytest
 
 from src.swagger_server.controllers.create import _add_responses, _query_degree, _query_bulk
-from .helpers import _get_short_response, _get_query_result
+from .helpers import _get_short_response, _get_query_result, _get_aggregate, _get_demographic_query_result
 
 
 def test_add_responses():
@@ -60,4 +60,31 @@ def test_query_degree(query_result):
     }]
 
     expected_output = _get_query_result()
+    assert(_query_degree(details) == expected_output)
+
+
+@mock.patch('pymongo.collection.Collection.aggregate')
+@mock.patch('src.swagger_server.controllers.create._query_bulk')
+def test_query_degree_demographics(query_result, mock_mongo_aggregate):
+    query_result.return_value = _get_short_response()
+    mock_mongo_aggregate.return_value = _get_aggregate()
+
+    details = [{
+        "university_id": "3f98f095ef1a7b782d9c897d8d004690d598ebe4c301f14256366beeaf083365",
+        "degree_id": "7c9a1789f207659f2a28ee16737946d6b4189cb507ddd0fedc92978acaba4dfa",
+        "degree_name": "Fintech",
+        "absolute": 2,
+        "race": ["Black", "Other", "Indian"],
+        "gender": ["Female", "Other"]
+    },
+    {
+        "university_id": "3f98f095ef1a7b782d9c897d8d004690d598ebe4c301f14256366beeaf083365",
+        "degree_id": "7c9a1789f207659f2a28ee16737946d6b4189cb507ddd0fedc92978acaba4dfb",
+        "degree_name": "Statistics",
+        "absolute": 2,
+        "race": ["Black", "Other", "Indian"],
+        "gender": ["Female", "Other"]
+    }]
+
+    expected_output = _get_demographic_query_result()
     assert(_query_degree(details) == expected_output)
