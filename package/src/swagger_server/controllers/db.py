6278aad785ada53f3be36fa24ea18556680573b6
Bill Guo <bill@ntls.io>
2023-02-14 16:23:23 +0200

    feat: increase connection and server selection timeout to 60s

--- package/src/swagger_server/controllers/db.py
+++ package/src/swagger_server/controllers/db.py
@@ -3,10 +3,12 @@ from os import environ as env
 from pymongo import MongoClient
 
 CLIENT = MongoClient(
-  'mongodb://mongodb:27017/', 
-  username=env.get('MONGO_USERNAME'), 
-  password=env.get('MONGO_PASSWORD')
-  )
+    'mongodb://mongodb:27017/',
+    username=env.get('MONGO_USERNAME'),
+    password=env.get('MONGO_PASSWORD'),
+    connectTimeoutMS=60000,
+    serverSelectionTimeoutMS=60000
+)
 DB = CLIENT.database
 query_details = DB.query_db
-student_details =  DB.student_db
+student_details = DB.student_db
